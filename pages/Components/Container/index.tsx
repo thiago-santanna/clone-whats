24d3d7561a3d047ae1f53153361a8482d821ab53
xthiagosant <uzmaki_thiago@outlook.com>
2021-06-17 15:44:06 -0300

    adicionado rolagem nas mensagens

--- pages/Components/Container/index.tsx
+++ pages/Components/Container/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Element } from 'react-scroll'
 import Notificacao from './Notificacao';
 import styles from './container.module.css'
 import HeaderContact from './HeaderContact'
@@ -15,22 +16,33 @@ const Container: React.FC = () => {
                     <Notificacao />   
                     <Consulta />
                     <div className={styles.msgContatos +  ` flex-1`}>
-                        <Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 2" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 3" statusMensagem="n" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 4" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 2" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 3" statusMensagem="n" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 4" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 2" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 3" statusMensagem="n" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 4" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 2" statusMensagem="v" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 3" statusMensagem="n" ultimaMensagem="Última mensagem..." />
-                        <Mensagem nome="Thiago 4" statusMensagem="v" ultimaMensagem="Última mensagem..." />
+                        <Element name="listMensagens" className="element" id="containerElement" style={{
+                        position: 'relative',
+                        height: '29rem',
+                        overflowY: 'scroll'
+                        }}>
+                            <Element name="lista1"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista2"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista3"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista4"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista5"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista6"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista7"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista8"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista9"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista10"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista11"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista12"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista13"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista14"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista15"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista16"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista17"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista18"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista19"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                            <Element name="lista20"><Mensagem nome="Thiago 1" statusMensagem="v" ultimaMensagem="Última mensagem..." /></Element>
+                        </Element>
+
                     </div>
                 </div>
 
