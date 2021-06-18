5191140eacc89b4c6ff433180ad415106b3024e7
xthiagosant <uzmaki_thiago@outlook.com>
2021-06-18 11:31:15 -0300

    componente de escrever mensagem

--- pages/Components/Container/EnviarMensagem/index.tsx
+++ pages/Components/Container/EnviarMensagem/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FiSmile, FiPaperclip, FiMic } from 'react-icons/fi'
 
 const EnviarMensagem = () => {
     return (
         <>
-            <div>
-                <h1>Escrever mensagem</h1>
+            <div className="flex items-center gap-3 py-3 bg-gray-100">
+                <FiSmile className="ml-3" size={22} color="#919191" />
+                <FiPaperclip size={22} color="#919191" />
+                <div className="flex flex-grow bg-white p-1 rounded-3xl">
+                    <input className="flex-1" type="text" />
+                </div>
+                <FiMic className="mr-3" size={22} color="#919191" />
             </div>
         </>
     );
